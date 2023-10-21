21c21adeda77c3a1797a2a29c268ec72fbc6fd16
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-10-21 21:41:14 +0530

    todo challenge

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
+import { useRef, useState } from "react";
 import "./App.css";
-import Slider from "./Slider";
+import Item from "./Item";
 
 function App() {
-  const images = [
-    {
-      image_url:
-        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
-      caption: "Image 1",
-    },
-    {
-      image_url:
-        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
-      caption: "Image 2",
-    },
-    {
-      image_url:
-        "https://img.freepik.com/free-photo/young-lady-shirt-jacket-making-scales-gesture-looking-cheerful-front-view_176474-85195.jpg?t=st=1693037931~exp=1693038531~hmac=2f83b6689538e4056912c96f448163e9ef10998f48f671b7e50279f81611fbe6",
-      caption: "Image 3",
-    },
-    {
-      image_url:
-        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
-      caption: "Image 4",
-    },
-    {
-      image_url:
-        "https://img.freepik.com/free-photo/young-lady-shirt-jacket-making-scales-gesture-looking-cheerful-front-view_176474-85195.jpg?t=st=1693037931~exp=1693038531~hmac=2f83b6689538e4056912c96f448163e9ef10998f48f671b7e50279f81611fbe6",
-      caption: "Image 5",
-    },
-    {
-      image_url:
-        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
-      caption: "Image 6",
-    },
-  ];
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      setTodos([
+        ...todos,
+        {
+          text: e.target.value,
+          completed: false,
+          id: Date.now(),
+        },
+      ]);
+      inputRef.current.value = "";
+    }
+  };
+
+  const handleCompleted = (id) => {
+    const updatedList = todos.map((e) => {
+      if (e.id === id) {
+        e.completed = !e.completed;
+      }
+      return e;
+    });
+    setTodos(updatedList);
+  };
+
+  const deleteTodo = (id) => {
+    const filter = todos.filter((e) => e.id !== id);
+    setTodos(filter);
+  };
+
+  const editTodo = (id, text) => {
+    const updatedList = todos.map((e) => {
+      if (e.id === id) {
+        e.text = text;
+      }
+      return e;
+    });
+
+    setTodos(updatedList);
+  };
 
   return (
     <div className="App">
-      <Slider images={images} />
+      <input type="text" ref={inputRef} onKeyPress={handleKeyPress} />
+      {todos.map((e) => (
+        <Item
+          {...e}
+          key={e.id}
+          handleCompleted={handleCompleted}
+          deleteTodo={deleteTodo}
+          editTodo={editTodo}
+        />
+      ))}
     </div>
   );
 }
