e07824c591184d681f12304bcff2b57e168ccad6
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-10-21 15:59:36 +0530

    responsive caraousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Slider from "./Slider";
 
 function App() {
+  const images = [
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
+      caption: "Image 1",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
+      caption: "Image 2",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/young-lady-shirt-jacket-making-scales-gesture-looking-cheerful-front-view_176474-85195.jpg?t=st=1693037931~exp=1693038531~hmac=2f83b6689538e4056912c96f448163e9ef10998f48f671b7e50279f81611fbe6",
+      caption: "Image 3",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
+      caption: "Image 4",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/young-lady-shirt-jacket-making-scales-gesture-looking-cheerful-front-view_176474-85195.jpg?t=st=1693037931~exp=1693038531~hmac=2f83b6689538e4056912c96f448163e9ef10998f48f671b7e50279f81611fbe6",
+      caption: "Image 5",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
+      caption: "Image 6",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider images={images} />
     </div>
   );
 }
---
+++ src/Slide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Slide = ({ image_url, caption, active }) => {
+  return (
+    <div className={`slide ${active ? "active" : ""}`}>
+      <img src={image_url} alt={caption} />
+      <span>{caption}</span>
+    </div>
+  );
+};
+
+export default Slide;
