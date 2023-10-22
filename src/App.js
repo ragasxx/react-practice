d1afcb5cb5ec51c16e46d88c0f800d1a218a2533
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-10-22 11:19:53 +0530

    usetoggler hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
-import { useCopy } from "./useCopy";
+
+const useToggle = (arr, index = 0) => {
+  const [currIndex, setCurrIndex] = useState(index);
+
+  const toggler = useCallback(() => {
+    return setCurrIndex((prevIndex) =>
+      prevIndex >= arr.length - 1 ? 0 : prevIndex + 1
+    );
+  }, [arr]);
+  return [toggler, arr[currIndex]];
+};
 
 function App() {
-  const [value, setValue] = useState("");
-  const copy = useCopy();
-
+  const [toggler, currentValue] = useToggle([1, 2, 3, 4, 5], 2);
   return (
     <div className="App">
-      <div>
-        <textarea
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        ></textarea>
-      </div>
-      <button
-        onClick={() => {
-          copy(value);
-          setValue("");
-        }}
-      >
-        Copy{" "}
-      </button>
+      <h1>{currentValue}</h1>
+      <button onClick={toggler}>toggle</button>
     </div>
   );
 }
