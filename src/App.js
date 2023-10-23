cd8c7dfd6485e1e2d5086a84516a8389294a63d8
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-10-23 13:00:58 +0530

    nested comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import { useCallback, useState } from "react";
 import "./App.css";
 
-const useToggle = (arr, index = 0) => {
-  const [currIndex, setCurrIndex] = useState(index);
+const commentsData = [
+  {
+    name: "sagar",
+    text: "hello ji",
+    replies: [
+      {
+        name: "aman",
+        text: "theek h ji",
+        replies: [
+          {
+            name: "pawan",
+            text: "or aman kya haal",
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: "isha",
+        text: "bdiaaaaa hu sagar",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "deepak",
+    text: "kya hal h bhaiyo ke",
+    replies: [
+      {
+        name: "sagar",
+        text: "bdia bhaia",
+        replies: [],
+      },
+    ],
+  },
+];
 
-  const toggler = useCallback(() => {
-    return setCurrIndex((prevIndex) =>
-      prevIndex >= arr.length - 1 ? 0 : prevIndex + 1
-    );
-  }, [arr]);
-  return [toggler, arr[currIndex]];
-};
-
-function App() {
-  const [toggler, currentValue] = useToggle([1, 2, 3, 4, 5], 2);
+const Comment = ({ data }) => {
+  const { text, name } = data;
   return (
-    <div className="App">
-      <h1>{currentValue}</h1>
-      <button onClick={toggler}>toggle</button>
+    <div className="flex bg-gray-100 my-4">
+      <img
+        src="https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black-thumbnail.png"
+        alt="user pic"
+        className="h-12 w-12"
+      />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
     </div>
   );
-}
+};
+
+const CommentList = ({ commentsData }) => {
+  return commentsData.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentList commentsData={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
+const App = () => {
+  return (
+    <div>
+      <CommentList commentsData={commentsData} />
+    </div>
+  );
+};
 
 export default App;
